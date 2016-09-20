40e8eedfd183c254b576bd508f24c5500d06f406
han dongqiang <handongqiang@handemacbook-pro.local>
2016-09-20 11:33:32 +0800

    初始化项目

---
+++ git_Test/git_Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  git_Test
+//
+//  Created by han dongqiang on 16/9/20.
+//  Copyright © 2016年 han dongqiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ git_Test/git_Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  git_Test
+//
+//  Created by han dongqiang on 16/9/20.
+//  Copyright © 2016年 han dongqiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
